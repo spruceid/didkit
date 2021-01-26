9af226a9a2e9a63240036b1b30e8ef086d61b0f2
Charles E. Lehner <charles.lehner@spruceid.com>
2021-01-26 14:09:15 -0500

    Add did:web

--- lib/src/did_methods.rs
+++ lib/src/did_methods.rs
@@ -1,10 +1,14 @@
 use did_key::DIDKey;
+#[cfg(feature = "did-web")]
+use did_web::DIDWeb;
 use ssi::did::DIDMethods;
 
 lazy_static! {
     pub static ref DID_METHODS: DIDMethods<'static> = {
         let mut methods = DIDMethods::default();
         methods.insert(&DIDKey);
+        #[cfg(feature = "did-web")]
+        methods.insert(&DIDWeb);
         methods
     };
 }
