6e32c938ca69329b22d036930ec39dbae7201cb9
Simon Bihel <simon.bihel@spruceid.com>
2021-04-06 16:04:29 +0100

    Use wee_alloc

--- lib/web/src/lib.rs
+++ lib/web/src/lib.rs
@@ -1,3 +1,9 @@
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 use core::future::Future;
 
 use js_sys::Promise;
