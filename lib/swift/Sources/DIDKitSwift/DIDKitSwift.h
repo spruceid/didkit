5ea6b6afabb1356a650b1a6f9badbf312186ca98
Guilherme Souza <grsouza@pm.me>
2021-07-13 07:33:38 -0300

    Add swift wrapper library

---
+++ lib/swift/Sources/DIDKitSwift/DIDKitSwift.h
@@ -0,0 +1,18 @@
+//
+//  DIDKitSwift.h
+//  DIDKitSwift
+//
+//  Created by Guilherme Souza on 29/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DIDKitSwift.
+FOUNDATION_EXPORT double DIDKitSwiftVersionNumber;
+
+//! Project version string for DIDKitSwift.
+FOUNDATION_EXPORT const unsigned char DIDKitSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIDKitSwift/PublicHeader.h>
+
+#import "didkit.h"
