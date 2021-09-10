06a046b058a7c63235527ce4b82cd1d7d7acdbca
Charles E. Lehner <charles.lehner@spruceid.com>
2021-09-10 14:52:28 -0400

    Add DID Resolver Override (-R) option (#209)

--- cli/src/opts.rs
+++ cli/src/opts.rs
@@ -5,8 +5,11 @@ use didkit::{HTTPDIDResolver, SeriesResolver, DID_METHODS};
 #[derive(StructOpt, Debug, Clone, Default)]
 pub struct ResolverOptions {
     #[structopt(env, short = "r", long, parse(from_str = HTTPDIDResolver::new))]
-    /// DID Resolver HTTP(S) endpoint, for non-built-in DID methods.
+    /// Fallback DID Resolver HTTP(S) endpoint, for non-built-in DID methods.
     pub did_resolver: Option<HTTPDIDResolver>,
+    #[structopt(env, short = "R", long, parse(from_str = HTTPDIDResolver::new))]
+    /// Override DID Resolver HTTP(S) endpoint, for all DID methods.
+    pub did_resolver_override: Option<HTTPDIDResolver>,
 }
 
 impl ResolverOptions {
@@ -14,6 +17,9 @@ impl ResolverOptions {
         let mut resolvers = vec![DID_METHODS.to_resolver()];
         if let Some(http_did_resolver) = &self.did_resolver {
             resolvers.push(http_did_resolver);
+        }
+        if let Some(http_did_resolver) = &self.did_resolver_override {
+            resolvers.insert(0, http_did_resolver);
         }
         SeriesResolver { resolvers }
     }
