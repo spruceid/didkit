5d32f8736606379584c55bd4cf6b283c596595d3
Charles E. Lehner <charles.lehner@spruceid.com>
2022-01-11 15:11:44 -0500

    Add did:ion DID method

--- lib/src/did_methods.rs
+++ lib/src/did_methods.rs
@@ -3,14 +3,26 @@ use did_method_key::DIDKey;
 use did_onion::DIDOnion;
 use did_pkh::DIDPKH;
 // use did_sol::DIDSol;
+use did_ion::DIDION;
 use did_tz::DIDTz;
 use did_web::DIDWeb;
 use did_webkey::DIDWebKey;
-use ssi::did::DIDMethods;
+use ssi::did::{DIDMethod, DIDMethods};
+use std::env::VarError;
 
 lazy_static! {
     static ref DIDTZ: DIDTz = DIDTz::default();
     static ref DIDONION: DIDOnion = DIDOnion::default();
+    static ref ION: DIDION = DIDION::new(
+        match std::env::var("DID_ION_API_URL") {
+            Ok(string) => Some(string),
+            Err(VarError::NotPresent) => None,
+            Err(VarError::NotUnicode(err)) => {
+                eprintln!("Unable to parse DID_ION_API_URL: {:?}", err);
+                None
+            }
+        }
+    );
     pub static ref DID_METHODS: DIDMethods<'static> = {
         let mut methods = DIDMethods::default();
         methods.insert(&DIDKey);
@@ -21,6 +33,7 @@ lazy_static! {
         methods.insert(&DIDWebKey);
         methods.insert(&DIDPKH);
         methods.insert(&*DIDONION);
+        methods.insert(&*ION);
         methods
     };
 }
