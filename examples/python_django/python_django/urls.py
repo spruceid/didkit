f25bdfe4c76a3dfbcb75b2cf44dca8d5fea395da
Simon Bihel <simon.bihel@spruceid.com>
2022-08-16 09:17:00 +0100

    Move Python examples to didkit-python
    Close #307

--- examples/python_django/python_django/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('didkit/', include('didkit_django.urls')),
-    path('admin/', admin.site.urls),
-]
