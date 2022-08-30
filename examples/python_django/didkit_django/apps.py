fcb88820a92f326f78b258c8cead7bb63c5d9e3a
Simon Bihel <sbihel@users.noreply.github.com>
2022-08-30 11:37:28 +0100

    Move Python examples to didkit-python and fix CI (#308)
    * Move Python examples to didkit-python
    * Update ssi in CI and bump MSRV
    * Fix Android installation in CI and use debug compilation for faster CI time
    
    Close #307

--- examples/python_django/didkit_django/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DidkitDjangoConfig(AppConfig):
-    name = 'didkit_django'
--- examples/python_django/didkit_django/templatetags/qrcode.py
+++
@@ -1,9 +0,0 @@
-from qr_code.qrcode.maker import make_qr_code_with_args
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag()
-def qr_from_text(text, **kwargs) -> str:
-    return make_qr_code_with_args(text, qr_code_args=kwargs)
