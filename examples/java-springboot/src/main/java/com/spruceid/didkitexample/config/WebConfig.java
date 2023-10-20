08aeafe0eb737cbfb47b90aa66a20965dff3c113
CharlesShuller <CharlesShuller@users.noreply.github.com>
2023-10-20 17:37:57 -0500

    Add specific version for lombok and improved logging of Java Springboot Example (#371)
    * Add specific version for lombok
    
    * Added better logging

--- examples/java-springboot/src/main/java/com/spruceid/didkitexample/config/WebConfig.java
+++ examples/java-springboot/src/main/java/com/spruceid/didkitexample/config/WebConfig.java
@@ -3,10 +3,10 @@ package com.spruceid.didkitexample.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.slf4j.Logger;
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
--- examples/java-springboot/src/main/java/com/spruceid/didkitexample/controller/VersionController.java
+++ examples/java-springboot/src/main/java/com/spruceid/didkitexample/controller/VersionController.java
@@ -4,11 +4,17 @@ import com.spruceid.DIDKit;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 @RestController
 public class VersionController {
+    private static Logger logger = LogManager.getLogger();
 
     @RequestMapping("/version")
     public String version() {
+        logger.info("GET /version");
         return DIDKit.getVersion();
     }
 }
