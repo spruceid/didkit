640be935f5bde461fc7b69aa991b254e80a35b89
Charles Shuller <charles.shuller@spruceid.com>
2023-10-20 09:52:39 -0500

    Added better logging

--- examples/java-springboot/src/main/java/com/spruceid/didkitexample/user/UserController.java
+++ examples/java-springboot/src/main/java/com/spruceid/didkitexample/user/UserController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.time.Duration;
 import java.util.UUID;
 
@@ -22,20 +25,24 @@ import java.util.UUID;
 public class UserController {
     private final UserService userService;
     private final StringRedisTemplate redisTemplate;
+    private static Logger logger = LogManager.getLogger();
 
     @GetMapping("/sign-up")
     String signUpGet(User user) {
+        logger.info("GET /sign-up");
         return "sign-up";
     }
 
     @PostMapping("/sign-up")
     String signUpPost(User user) {
+        logger.info("POST /sign-up");
         userService.signUp(user);
         return "redirect:/sign-in";
     }
 
     @GetMapping("/sign-in")
     ModelAndView signIn() throws Exception {
+        logger.info("GET /sign-in");
         final String uuid = UUID.randomUUID().toString();
         final String url = "https://" + Resources.baseUrl + "/verifiable-presentation-request/" + uuid;
         final ModelAndView model = QRCode.getModelAndView("sign-in", url);
@@ -47,6 +54,7 @@ public class UserController {
     ModelAndView credential(
             @AuthenticationPrincipal User user
     ) throws Exception {
+        logger.info("GET /credential");
         final String uuid = UUID.randomUUID().toString();
         redisTemplate.opsForValue().set(uuid, user.getUsername());
         redisTemplate.expire(uuid, Duration.ofSeconds(90));
@@ -60,6 +68,7 @@ public class UserController {
             @RequestParam("did") String did,
             ModelAndView model
     ) throws Exception {
+        logger.info("POST /credential");
         final String credential = userService.issueCredential(did, user);
         model.addObject("credential", credential);
         return model;
