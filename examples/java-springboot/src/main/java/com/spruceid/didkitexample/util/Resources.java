29d96ab532702e2bc671451fdd0b3720af15bdd1
Charles Shuller <charles.shuller@spruceid.com>
2023-10-25 12:52:30 -0500

    Added logging, tests, and handling of clock skew between phone and server

--- examples/java-springboot/src/main/java/com/spruceid/didkitexample/util/Resources.java
+++ examples/java-springboot/src/main/java/com/spruceid/didkitexample/util/Resources.java
@@ -2,5 +2,5 @@ package com.spruceid.didkitexample.util;
 
 public class Resources {
     public static final String key = "key.jwk";
-    public static final String baseUrl = "example.com";
+    public static final String baseUrl = "open-actually-wahoo.ngrok-free.app";
 }
